be4fdc55442b41b4079a2af9c4b7f27d7a7cb837
Alisson Oliveira <binho_alisson@hotmail.com>
2023-07-22 21:16:37 -0300

    first commit

---
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.taskmanager.ui.theme.TaskManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskManagerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TaskCompletedApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskCompletedApp(){
+    TaskCompleted(
+        imageDone = painterResource(R.drawable.ic_task_completed),
+        title = "All tasks completed",
+        description = "Nice work!"
+    )
+}
+
+@Composable
+fun TaskCompleted(
+    imageDone: Painter,
+    title: String,
+    description: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = imageDone,
+            contentDescription = null
+        )
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = description,
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TaskCompletedAppPreview() {
+    TaskManagerTheme {
+        TaskCompletedApp()
+    }
+}
